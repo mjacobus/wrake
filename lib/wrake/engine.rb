df8aee40f8196aeb36d38b19d72fa70f268d4b7b
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2014-03-27 10:12:33 -0300

    Made rspec the testing framework of the engine

--- lib/wrake/engine.rb
+++ lib/wrake/engine.rb
@@ -1,5 +1,10 @@
 module Wrake
   class Engine < ::Rails::Engine
     isolate_namespace Wrake
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.integration_tool :rspec
+    end
   end
 end
